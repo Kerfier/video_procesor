bb9293ddbb8f03324230bbce5dc6cb41dd4b5783
Dzmitry Lukashuk <lukashukdn@gmail.com>
2026-03-25 15:27:23 +0100

    Add installation tests.

--- install.bat
+++ install.bat
@@ -2,8 +2,13 @@
 setlocal
 
 python -m pip install --upgrade pip
+if errorlevel 1 exit /b 1
+
 python -m pip install .
+if errorlevel 1 exit /b 1
+
 python -m pip uninstall opencv-python -y 2>nul
 python -m pip install opencv-contrib-python --force-reinstall -q
+if errorlevel 1 exit /b 1
 
 echo Installation complete. Run: video-processor --help
